b0c8e3d76c9bea1a0968e521355de0526caf0b9b
Ruona Clinton <66035235+clintonomamohwo@users.noreply.github.com>
2026-04-22 02:19:36 -0400

    test

--- src/app/components/AboutPage.tsx
+++ src/app/components/AboutPage.tsx
@@ -1010,7 +1010,7 @@ export function AboutPage() {
       />
       <div
         className="relative overflow-hidden"
-        style={{ background: "#4b1a34" }}
+        style={{ background: "#1A1F4B" }}
       >
         {/* Background layers */}
         <div
